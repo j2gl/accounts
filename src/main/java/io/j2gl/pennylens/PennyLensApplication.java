4dfcd043c2a915f00b7c010b099e0baf3df48e87
j2gl <j2gl.dev@gmail.com>
2025-07-27 21:21:20 +0200

    Add java structure with spring boot

---
+++ src/main/java/io/j2gl/pennylens/PennyLensApplication.java
@@ -0,0 +1,13 @@
+package io.j2gl.pennylens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennyLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennyLensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/j2gl/pennylens/PennyLensApplicationTests.java
@@ -0,0 +1,13 @@
+package io.j2gl.pennylens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennyLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
